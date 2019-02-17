8f921bc4311e4633298b8ebcd2259cc4ba4aa9d3
Casey <caseymarquis@gmail.com>
2019-02-16 20:19:23 -0500

    Force HTTP to HTTPS redirection when ASPNETCORE_HTTPS_PORT is set.

--- App/Web/AppWebService.cs
+++ App/Web/AppWebService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,7 +9,9 @@ using Microsoft.Extensions.FileProviders;
 using Swashbuckle.AspNetCore.Swagger;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Text;
 
 namespace QApp.Web
@@ -23,7 +26,17 @@ namespace QApp.Web
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            services.Configure<ForwardedHeadersOptions>(options => {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.All;
+                options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("::ffff:0.0.0.0"), 0));
+            });
             services.AddMvc();
+            services.AddHsts(options => {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(60);
+            });
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new Info { Title = "QApp API", Version = "v1" });
             });
@@ -33,9 +46,15 @@ namespace QApp.Web
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
-#if DEBUG
+            if (!Debugger.IsAttached) {
+                app.UseForwardedHeaders();
+                app.UseHttpsRedirection();
+                app.UseHsts();
+            }
+            else {
                 app.UseDeveloperExceptionPage();
-#endif
+            }
+
             app.UseDefaultFiles();
 
             app.UseStaticFiles();
@@ -61,14 +80,16 @@ namespace QApp.Web
             app.UseSwaggerUI(c => {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "QApp API v1");
             });
-#if DEBUG
-            app.UseCors(builder => builder
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowAnyOrigin()
-            .AllowCredentials()
-            );
-#endif
+
+            if (Debugger.IsAttached) {
+                app.UseCors(builder => builder
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin()
+                .AllowCredentials()
+                );
+            }
+
             app.UseMvc();
             app.UseSignalR(routes => {
                 //routes.MapHub<UpdateHub>("updates");
