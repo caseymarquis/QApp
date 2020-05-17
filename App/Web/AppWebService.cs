de735317e967c103e4de5b4eeafea08aaf92a2a4
Casey Marquis <caseymarquis@gmail.com>
2020-05-16 22:27:42 -0400

    Revert AspNetCore to JSON.NET

--- App/Web/AppWebService.cs
+++ App/Web/AppWebService.cs
@@ -64,7 +64,7 @@ namespace QApp.Web
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "QApp API", Version = "v1" });
             });
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddSignalR(c => {
                 c.EnableDetailedErrors = true;
             });
